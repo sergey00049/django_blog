bbb13bf04d8076a1c7f833df2ae2d71fba741960
gray00049 <gray00049@bk.ru>
2019-11-24 00:39:59 +0300

    Change settings.py

--- blog_training/settings.py
+++ blog_training/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'vqcj3z)ri7+47c)$9&x)c5%9y)v(#iglpm+tj+=-$^&!k*i9&j')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'https://shrouded-cliffs-40121.herokuapp.com/',
